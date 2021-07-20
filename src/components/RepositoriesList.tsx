a2d0e125f20e9c5ccadcaaba444f4ac65be4fc45
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2021-07-20 22:25:43 +0700

    38. Calling an Action Creator

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { actionCreators } from '../state';
 
 const RepositoriesList: React.FC = () => {
     const [term, setTerm] = useState('');
-    const onSubmit = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const dispatch = useDispatch();
+
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        dispatch(actionCreators.searchRepositories(term));
     }
     return (
         <form onSubmit={(e) => onSubmit(e)}>
