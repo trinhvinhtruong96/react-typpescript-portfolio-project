d081456336036b7587dc4d81657883251ccf7b69
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2021-07-20 22:53:09 +0700

    42. Creating a Typed Selector

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
 import { useActions } from '../hooks/useActions';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 
 const RepositoriesList: React.FC = () => {
     const [term, setTerm] = useState('');
     const { searchRepositories } = useActions();
-    const { data, error, loading } = useSelector((state) => state.repositories);
+    const { data, error, loading } = useTypedSelector((state) => state.repositories);
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
