511a3ad0ef78079086399ee2af06f23d37b3a6fa
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2021-07-20 23:00:07 +0700

    43. Consuming Store State

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -19,7 +19,7 @@ const RepositoriesList: React.FC = () => {
             </form>
             {error && <h3>{error}</h3>}
             {loading && <h3>Loading...</h3>}
-            {!error && !loading && data}
+            {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
         </div>
     )
 }
