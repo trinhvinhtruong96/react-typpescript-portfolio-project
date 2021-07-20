9698307fc86a2230ecfc019c9863924857e8afc5
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2021-07-20 11:36:24 +0700

    Section 3: Types Around Events and Refs

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import ReactDom from 'react-dom';
-import GuestList from './state/GuestList';
-import UserSearch from './state/UserSearch';
+import UserSearch from './refs/UserSearch';
+// import EventComponent from './events/EventComponent';
+// import GuestList from './state/GuestList';
+// import UserSearch from './state/UserSearch';
 
 const App = () => {
     return (
         <div>
             <h1>Hi there !</h1>
+            {/* <EventComponent /> */}
             <UserSearch />
             {/* <GuestList /> */}
         </div>
