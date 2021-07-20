e885a44163111d0c83baabb9c27e3f336ee5c752
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2021-07-20 09:34:31 +0700

    Create app

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import ReactDom from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hi there !</h1>
+        </div>
+    )
+}
+
+ReactDom.render(
+    <App />,
+    document.querySelector('#root')
+);
