f60a6b90a599632ee97a9ad0ec5843d03764dc3f
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2021-07-22 21:50:12 +0700

    Section 9: Caching For Big Performance Gains

--- src/plugins/fetch-pluggin.ts
+++ src/plugins/fetch-pluggin.ts
@@ -19,32 +19,46 @@ export const fetchPlugin = (inputCode: string) => {
 
             build.onLoad({ filter: /.*/ }, async (args: any) => {
                 // if package in cache return
-                // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+                const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
-                // if (cachedResult) {
-                //     return cachedResult;
-                // }
+                if (cachedResult) {
+                    return cachedResult;
+                }
+            });
 
+            build.onLoad({ filter: /.css$/ }, async (args: any) => {
                 // if not request and get result to return
                 const { data, request } = await axios.get(args.path);
-
-                const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
 
                 const escaped = data
                     .replace(/\n/g, '')
                     .replace(/"/g, '\\"')
                     .replace(/'/g, "\\'");
 
-                const contents = fileType === 'css' ?
-                    `
+                const contents = `
                     const style = document.createElement('style');
                     style.innerText = '${escaped}';
                     document.head.appendChild(style);
-                ` : data;
+                `;
 
                 const result: esbuild.OnLoadResult = {
                     loader: 'jsx',
                     contents,
+                    resolveDir: new URL('./', request.responseURL).pathname
+                }
+
+                await fileCache.setItem(args.path, result);
+
+                return result;
+            });
+
+            build.onLoad({ filter: /.*/ }, async (args: any) => {
+                // if not request and get result to return
+                const { data, request } = await axios.get(args.path);
+
+                const result: esbuild.OnLoadResult = {
+                    loader: 'jsx',
+                    contents: data,
                     resolveDir: new URL('./', request.responseURL).pathname
                 }
 
