c2f8daa700ed62d33f291c6991eb9c1b6bcd6ef6
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2021-07-22 21:17:28 +0700

    92. Escaping CSS Snippets - beffore

---
+++ src/plugins/fetch-pluggin.ts
@@ -0,0 +1,57 @@
+import * as esbuild from 'esbuild-wasm';
+import localForage from 'localforage';
+import axios from 'axios';
+
+const fileCache = localForage.createInstance({
+    name: 'filecache'
+});
+export const fetchPlugin = (inputCode: string) => {
+    return {
+        name: 'fetch-plugin',
+        setup(build: esbuild.PluginBuild) {
+
+            build.onLoad({ filter: /(^index\.js$)/ }, () => {
+                return {
+                    loader: 'jsx',
+                    contents: inputCode,
+                };
+            });
+
+            build.onLoad({ filter: /.*/ }, async (args: any) => {
+                // if package in cache return
+                // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+                // if (cachedResult) {
+                //     return cachedResult;
+                // }
+
+                // if not request and get result to return
+                const { data, request } = await axios.get(args.path);
+
+                const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+
+                const escaped = data
+                    .replace(/\n/g, '')
+                    .replace(/"/g, '\\"')
+                    .replace(/'/g, "\\'");
+
+                const contents = fileType === 'css' ?
+                    `
+                    const style = document.createElement('style');
+                    style.innerText = '${escaped}';
+                    document.head.appendChild(style);
+                ` : data;
+
+                const result: esbuild.OnLoadResult = {
+                    loader: 'jsx',
+                    contents,
+                    resolveDir: new URL('./', request.responseURL).pathname
+                }
+
+                await fileCache.setItem(args.path, result);
+
+                return result;
+            });
+        }
+    }
+}
