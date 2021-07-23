b4c5e5fc02477572475107e8e6cd35c0df2f8136
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2021-07-23 23:59:27 +0700

    Section 10: Safely Handling Untrusted Code Execution

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,8 @@ import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
     const ref = useRef<any>();
+    const iframe = useRef<any>();
     const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
 
     const startService = async () => {
         ref.current = await esbuild.startService({
@@ -27,6 +27,8 @@ const App = () => {
         //     target: 'es2015'
         // });
 
+        iframe.current.srcdoc = html;
+
         const result = await ref.current.build({
             entryPoints: ['index.js'],
             bundle: true,
@@ -41,8 +43,29 @@ const App = () => {
             }
         })
 
-        setCode(result.outputFiles[0].text);
+        // setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
     }
+
+    const html = `
+      <html>
+        <head>
+            <body>
+                <div id="root"></div>
+                <script>
+                    window.addEventListener('message', (event)=> {
+                        try {
+                            eval(event.data);
+                        } catch (err) {
+                            const root = document.querySelector('#root');
+                            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+                        }
+                    }, false);
+                </script>
+            </body>
+        </head>
+      </html>
+    `;
 
     useEffect(() => {
         startService()
@@ -56,7 +79,7 @@ const App = () => {
                     Submit
                 </button>
             </div>
-            <pre>{code}</pre>
+            <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
         </div>
     )
 }
