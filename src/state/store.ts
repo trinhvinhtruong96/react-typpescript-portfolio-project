406893a32d6fcd3279282016b061354005fa43ec
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2021-07-27 11:27:14 +0700

    remove unuse code

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,5 @@
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
-import { ActionType } from "./action-types";
 import reducers from "./reducers";
 
 export const store = createStore(
